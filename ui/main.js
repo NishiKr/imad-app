547d409c03827ac4c1faad434620a852daf11c52
Nishanth Kr <nishikr5@gmail.com>
2017-09-19 20:31:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,2 +1,15 @@
 console.log('Loaded!');
     
+var element = document.getElementById('main-text');
+element.innerHTML='New Value';
+
+var image = document.getElementById('madi');
+var marginLeft = 0;
+function moveRight()
+{
+    marginLeft = marginLeft + 1;
+    img.style.marginLeft = marginLeft + 'px';
+}
+img.onclick = function(){
+    var interval = setInterval(moveRight, 50);
+};
